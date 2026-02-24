372f5a515981f7510fda56552c8e024042825da6
victor-tech-world <vladworld02@gmail.com>
2026-02-24 14:56:39 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      mainblue: '#146ef5',
+      header: '#222'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
