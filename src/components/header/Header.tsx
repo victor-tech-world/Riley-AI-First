ed4b0b291927dfb36c9f6fff49e7241334a4e20a
victor-tech-world <vladworld02@gmail.com>
2026-02-24 14:58:11 +0800

    test commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,20 +3,20 @@ import logo from "../../assets/logo.svg"
 export default function Header() {
     return (
         <>
-        <div className="flex justify-between pt-7 pr-4 pl-4">
-            <div className="flex pl-4">
-                <img className="w-20 h-20" src="../../" alt="Img" />
-                {/* dfsfds */}
+            <div className="flex justify-between pt-7 pr-4 pl-4">
+                <div className="flex pl-4">
+                    <img className="w-20 h-20" src="../../" alt="Img" />
+                    {/* dfsfds */}
+                </div>
+
+                <div className=" flex">
+                    <div className="pr-4 text-header">Use Cases</div>
+                    <div className="pr-4 text-header">Product</div>
+                    <div className="pr-4 text-header">Resources</div>
+                    <div className="pr-4 text-header">Requset a Demo</div>
+                    <div >Login test</div>
+                </div>
             </div>
-            
-            <div className=" flex">
-                <div className="pr-4 text-header">Use Cases</div>
-                <div className="pr-4 text-header">Product</div>
-                <div className="pr-4 text-header">Resources</div>
-                <div className="pr-4 text-header">Requset a Demo</div>
-                <div >Login</div>
-            </div>
-        </div>
         </>
     )
 }
